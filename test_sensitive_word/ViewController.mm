4ab899b5309394e67540bd8d3e9766640679edff
AsteriskZuo <zuoyuhsywn@hotmail.com>
2019-12-08 00:40:10 +0800

    实现敏感字过滤

--- test_sensitive_word/ViewController.mm
+++ test_sensitive_word/ViewController.mm
@@ -7,6 +7,8 @@
 //
 
 #import "ViewController.h"
+#include <SensitiveWord/SensitiveWord.hpp>
+#import <opencc/BundleTools.h>
 
 @interface ViewController ()
 
@@ -17,6 +19,17 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view.
+    [self test];
+}
+
+- (void)test {
+    zy::test_minganci_res("");
+}
+
+- (void)test2 {
+    NSBundle* bundle = [BundleTools getBundle];
+    NSString* url = [BundleTools getBundlePath:@"t2s.json"];
+    int a = 0;
 }
 
 
